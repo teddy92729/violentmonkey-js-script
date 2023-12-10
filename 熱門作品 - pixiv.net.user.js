768da6d89394d33312cc2ac396bbf390694f98e2
HUANG, YUN-TAI <teddy92729@gmail.com>
2023-12-11 03:38:21 +0800

    Update 熱門作品 - pixiv.net.user.js

--- 熱門作品 - pixiv.net.user.js
+++ 熱門作品 - pixiv.net.user.js
@@ -3,20 +3,20 @@
 // @namespace   Violentmonkey Scripts
 // @match       http*://www.pixiv.net/*
 // @grant       none
-// @version     2.0
+// @version     2.1
 // @author      -
 // @run-at      document-start
 // @require     https://teddy92729.github.io/tool.js
 // @description 2021/12/25 下午3:47:33
 // ==/UserScript==
 addCssDisplayNone(
-    "section > div > a[href^=\"/premium/lead/lp/\"]", // 阻擋premium超連結
-    "section > div:has(a[href^=\"/premium/lead/lp/\"]) > aside > iframe",
-    "section > div:has(a[href^=\"/premium/lead/lp/\"]) > aside > div", // 阻擋premium廣告文字
-    "section > div:has(a[href^=\"/premium/lead/lp/\"]) > aside > button", // 阻擋premium廣告按鈕
+    "section div > a[href^=\"/premium/lead/lp/\"]", // 阻擋premium超連結
+    "section div:has(a[href^=\"/premium/lead/lp/\"]) > aside > iframe",
+    "section div:has(a[href^=\"/premium/lead/lp/\"]) > aside > div", // 阻擋premium廣告文字
+    "section div:has(a[href^=\"/premium/lead/lp/\"]) > aside > button", // 阻擋premium廣告按鈕
 );
 addCss(`
-    section > div:has(a[href^=\"/premium/lead/lp/\"]) > aside > ul {
+    section div:has(a[href^=\"/premium/lead/lp/\"]) > aside > ul {
         mask: unset;
         overflow-x: overlay;
 
@@ -38,11 +38,11 @@ addCss(`
 
 let main = (e) => {
     // 檢查網站位置
-    if (/tags\/\S*\/artworks/.test(window.location.pathname)) {
+    if (/tags\/\S*/.test(window.location.pathname)) {
         let tags = window.location.pathname.split('/')[2];//取得tags
         console.log(tags);
 
-        elementCreated("section > div:has(a[href^=\"/premium/lead/lp/\"]) > aside:has(img)") // 等待熱門作品元素生成
+        elementCreated("section div:has(a[href^=\"/premium/lead/lp/\"]) > aside:has(img)") // 等待熱門作品元素生成
             .then((aside) => {
                 let ul = aside.querySelector("ul");
                 let template_li = ul.children[0].cloneNode(true);//取得第一個熱門作品元素作為模板
